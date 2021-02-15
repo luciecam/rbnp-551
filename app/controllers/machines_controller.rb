c699a18170c12f1176d24fa76bea81c2f14b305f
Alexandre Slim <alexandreslim@yahoo.fr>
2021-02-15 18:07:49 +0100

    index, show of machines and its controller

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,2 +1,30 @@
 class MachinesController < ApplicationController
+
+	def index
+		@machines = Machine.all
+	end
+
+	def show
+		@machine = Machine.find(params[:id])
+	end
+
+	def new
+		@machine = Machine.new
+	end
+
+	def create
+		@machine = Machine.new(machine_params)
+		if @machine.save
+			redirect_to machine_path(@machine)
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def machine_params
+		params.require(:machine).permit(:name, :description, :specifications, :price, :address, :condition)
+	end    
 end
+
